11108a4862d12923cafca5eed664c78480fa49d5
EduardoBelianCin <eclb@cin.ufpe.br>
2026-03-02 15:17:12 -0300

    First Commit

---
+++ Random/PoQueMao.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define FORi(n) for(int i=0;i<n;i++)
+
+void main() {
+    int n,x,y,z; scanf("%d %d %d %d", &n,&x,&y,&z);
+    int v[] = {x,y,z};
+
+    if(v[0] > v[1]) { int temp = v[0]; v[0] = v[1]; v[1] = temp; }
+    if(v[1] > v[2]) { int temp = v[1]; v[1] = v[2]; v[2] = temp; }
+    if(v[0] > v[1]) { int temp = v[0]; v[0] = v[1]; v[1] = temp; }
+
+    int soma = 0, ans = 0;
+    FORi(3) {
+        soma += v[i];
+        if(soma <= n) { ans++; }
+        else { break; }
+    }
+    printf("%d\n", ans);
+}
