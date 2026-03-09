e6a3e504bb8e6772cd7247b1df0c1ace927443ee
EduardoBelianCin <eclb@cin.ufpe.br>
2026-03-08 22:05:29 -0300

    Correcoes

--- Listas/L1/B.c
+++ Listas/L1/B.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int n; scanf("%d", &n);
-    int a,b; scanf("%d %d", &a, &b);
+    long long n; scanf("%lld", &n);
+    long long a,b; scanf("%lld %lld", &a, &b);
     
-    int soma = (n*(n+1))/2;
+    long long soma = (n*(n+1))/2;
 
-    int qtd_mult_a = n/a;
-    int soma_mult_a = ((qtd_mult_a+1)*a*qtd_mult_a)/2;
+    long long qtd_mult_a = n/a;
+    long long soma_mult_a = ((qtd_mult_a+1)*a*qtd_mult_a)/2;
 
-    int qtd_mult_b = n/b;
-    int soma_mult_b = ((qtd_mult_b+1)*b*qtd_mult_b)/2;
+    long long qtd_mult_b = n/b;
+    long long soma_mult_b = ((qtd_mult_b+1)*b*qtd_mult_b)/2;
 
-    int ab = a*b;
-    int qtd_mult_ab = n/ab;
-    int soma_mult_ab = ((qtd_mult_ab+1)*ab*qtd_mult_ab)/2;
+    long long ab = a*b;
+    long long qtd_mult_ab = n/ab;
+    long long soma_mult_ab = ((qtd_mult_ab+1)*ab*qtd_mult_ab)/2;
 
-    int ans = soma - soma_mult_a - soma_mult_b + soma_mult_ab;
-    printf("%d\n", ans);
+    long long ans = soma - soma_mult_a - soma_mult_b + soma_mult_ab;
+    printf("%lld\n", ans);
     if(ans%2 == 0) { printf("Lá ele!!!\n"); }
     else { printf("Opa xupenio AULAS...\n"); }
 
--- Listas/L1/C.c
+++ Listas/L1/C.c
@@ -14,36 +14,37 @@ int main() {
     else {
         int inc = 1; n++; rebeka--;
         qtd_x = (x*n)/100; qtd_y = (y*n)/100; qtd_z = (z*n)/100;
+        int resto = n - qtd_x - qtd_y - qtd_z;
 
-        if((x*n)%100 == 0 && (y*n)%100 == 0 && (z*n)%100 == 0) {
+        if(resto >= 2 && (x*n)%100 == 0 && (y*n)%100 == 0 && (z*n)%100 == 0) {
             printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", qtd_x, qtd_y, qtd_z);
-            int resto = n - qtd_x - qtd_y - qtd_z;
             rebeka += resto;
         }
         else {
             inc = 2; n++; rebeka--;
             qtd_x = (x*n)/100; qtd_y = (y*n)/100; qtd_z = (z*n)/100;
+            resto = n - qtd_x - qtd_y - qtd_z;
 
-            if((x*n)%100 == 0 && (y*n)%100 == 0 && (z*n)%100 == 0) {
+            if(resto >= 3 && (x*n)%100 == 0 && (y*n)%100 == 0 && (z*n)%100 == 0) {
                 printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", qtd_x, qtd_y, qtd_z);
 
                 char c1,c2,c3; scanf(" %c %c %c", &c1, &c2, &c3);
                 int soma = c1-'a'+1 + c2-'a'+1 + c3-'a'+1;
                 printf("%d\n", soma);
+                rebeka += resto;
             }
             else {
                 inc = 3; n++; rebeka--;
                 qtd_x = (x*n)/100; qtd_y = (y*n)/100; qtd_z = (z*n)/100;
+                resto = n - qtd_x - qtd_y - qtd_z;
 
-                if((x*n)%100 == 0 && (y*n)%100 == 0 && (z*n)%100 == 0) {
+                if(resto >= 4 && (x*n)%100 == 0 && (y*n)%100 == 0 && (z*n)%100 == 0) {
                     printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", qtd_x, qtd_y, qtd_z);
 
                     int i1,i2,i3; scanf(" %d %d %d", &i1, &i2, &i3);
                     int soma_parcelas = 0;
-                    if(i1%3 == 0) { soma_parcelas += i1/3; }
-                    if(i2%3 == 0) { soma_parcelas += i2/3; }
-                    if(i3%3 == 0) { soma_parcelas += i3/3; }
-                    printf("%d\n", soma_parcelas);
+                    if(i1%3 == 0 || i2%3 == 0 || i3%3 == 0) { soma_parcelas = i1/3 + i2/3 + i3/3; printf("%d\n", soma_parcelas); }
+                    rebeka += resto;
                 }
                 else {
                     printf("Nao foi dessa vez que Rebeka pode ajudar...\n");
