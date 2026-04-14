6f2fbd452da22d70fd7edf7d3542da024fcab4ed
EduardoBelianCin <eclb@cin.ufpe.br>
2026-04-13 23:03:36 -0300

    Quetao B e C feitas

--- Listas/L5/A.c
+++ Listas/L5/A.c
@@ -2,8 +2,6 @@
 #define FORi(n) for(int i=0;i<n;i++)
 
 int main() {
-    int a; scanf("%d", &a);
-    printf("%d\n", a);
 
     return 0;
 }
--- Listas/L5/B.c
+++ Listas/L5/B.c
@@ -9,56 +9,131 @@ float Media(float* Notas, int n) {
     return soma/n;
 }
 
-float MaiorNota(float* Notas, int n) {
+float MaiorNota(float* Notas, int* Indices, int n) {
     float max_nota = 0.0;
     int idx_max_nota = -1;
-    FORi(n) { if(Notas[i] > max_nota) { max_nota = Notas[i]; idx_max_nota = i+1; } }
+    FORi(n) { if(Notas[i] > max_nota) { max_nota = Notas[i]; idx_max_nota = Indices[i]; } }
     return max_nota;
 }
-int IdxMaiorNota(float* Notas, int n) {
+int IdxMaiorNota(float* Notas, int* Indices, int n) {
     float max_nota = 0.0;
     int idx_max_nota = -1;
-    FORi(n) { if(Notas[i] > max_nota) { max_nota = Notas[i]; idx_max_nota = i+1; } }
+    FORi(n) { if(Notas[i] > max_nota) { max_nota = Notas[i]; idx_max_nota = Indices[i]; } }
     return idx_max_nota;
 }
 
-float MenorNota(float* Notas, int n) {
+float MenorNota(float* Notas, int* Indices, int n) {
     float min_nota = 1000.0;
     int idx_min_nota = -1;
-    FORi(n) { if(Notas[i] < min_nota) { min_nota = Notas[i]; idx_min_nota = i+1; } }
+    FORi(n) { if(Notas[i] < min_nota) { min_nota = Notas[i]; idx_min_nota = Indices[i]; } }
     return min_nota;
 }
-int IdxMenorNota(float* Notas, int n) {
+int IdxMenorNota(float* Notas, int* Indices, int n) {
     float min_nota = 1000.0;
     int idx_min_nota = -1;
-    FORi(n) { if(Notas[i] < min_nota) { min_nota = Notas[i]; idx_min_nota = i+1; } }
+    FORi(n) { if(Notas[i] < min_nota) { min_nota = Notas[i]; idx_min_nota = Indices[i]; } }
     return idx_min_nota;
 }
 
+int AcimaMedia(float* Notas, int n, int media) {
+    int qtd_acima = 0;
+    FORi(n) { if(Notas[i] > media) { qtd_acima++; } }
+    return qtd_acima;
+}
+
+float Mediana(float* Notas, int n) {
+    if(n%2 == 1) { return Notas[n/2]; }
+    else { return (Notas[n/2]+Notas[n/2-1])/2; }
+}
+
+float Moda(float* Notas, int n) {
+    int Moda_Unica = 0;
+    int qtd_max = 0, prev = -1;
+    float moda;
+    int* freq = NULL;
+
+    int qtd = 0;
+    FORi(n) {
+        float nota = Notas[i];
+        if(nota != prev) {
+            qtd++;
+            int* temp_freq = realloc(freq, qtd * sizeof(float));
+
+            if(temp_freq == NULL) {
+                free(freq);
+                return 1;
+            }
+            
+            freq = temp_freq;
+            freq[qtd-1] = 1;
+        }
+        else { freq[qtd-1]++; }
+
+        // Analisa as freq
+        if(freq[qtd-1] > qtd_max) {
+            qtd_max = freq[qtd-1];
+            moda = nota;
+            Moda_Unica = 1;
+        }
+        else if(freq[qtd-1] == qtd_max && nota != moda) {
+            Moda_Unica = 0;
+        }
+
+        prev = nota;
+    }
+
+    if(Moda_Unica) { return moda; }
+    else { return -1.0; }
+}
+
+
 int main() {
     int n,k;
-    float* Notas = NULL;
-    int* Indices = NULL;
 
     // Valores iniciais
     scanf("%d", &n);
-    Notas = realloc(Notas, n * sizeof(int));
-    Indices = realloc(Indices, n * sizeof(int));
+    float* Notas = malloc(n * sizeof(float));
+    int* Indices = malloc(n * sizeof(int));
     if(Notas == NULL || Indices == NULL) {
+        printf("ERROU!\n");
         free(Notas);
         free(Indices);
         return 1;
     }
 
     // Notas
-    int acima_media = 0, max_indice, min_indice;
-    float media, max_nota = 0.0, min_nota = 1000.0, mediana, moda;
+    int acima_media, max_indice, min_indice;
+    float media, max_nota, min_nota, mediana, moda = -1.0;
 
     float x;
     FORi(n) {
         scanf("%f", &x);
+        Notas[i] = x;
         Indices[i] = i+1;
     }
+    // Ordena Crescente
+    FORi(n) {
+        FORj(n-i-1) {
+            if(Notas[j] > Notas[j+1]) {
+                float temp_n = Notas[j];
+                Notas[j] = Notas[j+1];
+                Notas[j+1] = temp_n;
+
+                int temp_i = Indices[j];
+                Indices[j] = Indices[j+1];
+                Indices[j+1] = temp_i;
+            }
+        }
+    }
+
+    media = Media(Notas, n);
+    max_nota = MaiorNota(Notas, Indices, n);
+    max_indice = IdxMaiorNota(Notas, Indices, n);
+    min_nota = MenorNota(Notas, Indices, n);
+    min_indice = IdxMenorNota(Notas, Indices, n);
+    acima_media = AcimaMedia(Notas, n, media);
+    mediana = Mediana(Notas, n);
+    moda = Moda(Notas, n);
 
     printf("Relatorio inicial\n");
     printf("Media: %.2f\n", media);
@@ -66,15 +141,14 @@ int main() {
     printf("Menor nota: %.2f (aluno %d)\n", min_nota, min_indice);
     printf("Acima da media: %d\n", acima_media);
     printf("Mediana: %.2f\n", mediana);
-    if(1 == 0) { printf("Moda: %.2f\n", moda); }
+    if(moda != -1.0) { printf("Moda: %.2f\n", moda); }
     else { printf("Moda: Nao ha moda unica\n"); }
 
 
     // Valores novos
     scanf("%d", &k);
-    n += k;
-    Notas = realloc(Notas, n * sizeof(int));
-    Indices = realloc(Indices, n * sizeof(int));
+    Notas = realloc(Notas, (n+k) * sizeof(float));
+    Indices = realloc(Indices, (n+k) * sizeof(int));
     if(Notas == NULL || Indices == NULL) {
         free(Notas);
         free(Indices);
@@ -83,16 +157,41 @@ int main() {
 
     FORi(k) {
         scanf("%f", &x);
-        Indices[i] = i+1;
+        Notas[n+i] = x;
+        Indices[n+i] = n+i+1;
+    }
+    n += k;
+    // Ordena Crescente
+    FORi(n) {
+        FORj(n-i-1) {
+            if(Notas[j] > Notas[j+1]) {
+                float temp_n = Notas[j];
+                Notas[j] = Notas[j+1];
+                Notas[j+1] = temp_n;
+
+                int temp_i = Indices[j];
+                Indices[j] = Indices[j+1];
+                Indices[j+1] = temp_i;
+            }
+        }
     }
 
-    printf("Relatorio atualizado\n");
+    media = Media(Notas, n);
+    max_nota = MaiorNota(Notas, Indices, n);
+    max_indice = IdxMaiorNota(Notas, Indices, n);
+    min_nota = MenorNota(Notas, Indices, n);
+    min_indice = IdxMenorNota(Notas, Indices, n);
+    acima_media = AcimaMedia(Notas, n, media);
+    mediana = Mediana(Notas, n);
+    moda = Moda(Notas, n);
+
+    printf("\nRelatorio atualizado\n");
     printf("Media: %.2f\n", media);
     printf("Maior nota: %.2f (aluno %d)\n", max_nota, max_indice);
     printf("Menor nota: %.2f (aluno %d)\n", min_nota, min_indice);
     printf("Acima da media: %d\n", acima_media);
     printf("Mediana: %.2f\n", mediana);
-    if(1 == 0) { printf("Moda: %.2f\n", moda); }
+    if(moda != -1.0) { printf("Moda: %.2f\n", moda); }
     else { printf("Moda: Nao ha moda unica\n"); }
 
     return 0;
